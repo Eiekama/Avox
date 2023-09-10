108bd8f73baafe2e73f54e76d562bb68f8050004
RandomAccount1128 <zheintzleman@gmail.com>
2023-09-10 16:08:48 -0400

    Meeting 1

---
+++ Assets/Scripts/Miscellaneous/Spikes.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour, IContactDamage
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    void DealContactDamage(PlayerInstance player)
+    {
+        //Lose HP from PlayerData from PlayerInstance
+        //Teleport to most recent checkpoint (can store in player?)
+    }
+}
