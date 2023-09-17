857585cb32f32263225e70f85bb0253188c14f5a
Joshua Charat-Collins <jcharatcollins@gmail.com>
2023-09-17 15:09:56 -0400

    Fixing bug in max speed force clamping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -24,16 +24,12 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        float runInput = _player.playerInputActions.Run.ReadValue<float>();
-        _player.RB.AddForce(new Vector2(runInput, 0) * 5.0f, ForceMode2D.Force);
         runWithMaxSpeed();
     }
 
     private void runWithMaxSpeed()
     {
         float runInput = _player.playerInputActions.Run.ReadValue<float>();
-        _player.RB.AddForce(new Vector2(runInput, 0) * 5.0f, ForceMode2D.Force);
-        
         float currSpeed = Vector2.SqrMagnitude(_player.RB.velocity);
         float maxSpeed = _player.maxSpeed * _player.maxSpeed;
         
@@ -43,8 +39,14 @@ public class PlayerController : MonoBehaviour
 
             Vector2 normalisedVelocity = _player.RB.velocity.normalized;
             Vector2 brakeVelocity = normalisedVelocity * brakeSpeed;  // make the brake Vector2 value
+            Debug.Log(brakeSpeed);
+            Debug.Log(currSpeed);
 
-            _player.RB.AddForce(-brakeVelocity, ForceMode2D.Force);  // apply opposing brake force
+            _player.RB.AddForce(-brakeVelocity + (new Vector2(runInput, 0) * 5.0f), ForceMode2D.Force);  // apply opposing brake force
+        }
+        else
+        {
+            _player.RB.AddForce(new Vector2(runInput, 0) * 5.0f, ForceMode2D.Force);  // apply opposing brake force
         }
     }
 }
