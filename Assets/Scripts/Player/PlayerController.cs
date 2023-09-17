f52e4cf8eeef732bd8bff192c152e9d59037a46e
Joshua Charat-Collins <jcharatcollins@gmail.com>
2023-09-17 14:57:20 -0400

    Add basic run with maximum velocity

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
 
 public class PlayerController : MonoBehaviour
 {
@@ -21,13 +23,28 @@ public class PlayerController : MonoBehaviour
                 _player.currentInteractable.Interact(_player);
             }
         }
+
+        float runInput = _player.playerInputActions.Run.ReadValue<float>();
+        _player.RB.AddForce(new Vector2(runInput, 0) * 5.0f, ForceMode2D.Force);
+        runWithMaxSpeed();
     }
 
-    public void onRun(InputAction.CallbackContext context)
+    private void runWithMaxSpeed()
     {
-        float movementInput = context.ReadValue<float>();
-        Vector2 movementParsed = new Vector2(movementInput, 0);
-        _player.movement.Run(movementParsed);
-        return;
+        float runInput = _player.playerInputActions.Run.ReadValue<float>();
+        _player.RB.AddForce(new Vector2(runInput, 0) * 5.0f, ForceMode2D.Force);
+        
+        float currSpeed = Vector2.SqrMagnitude(_player.RB.velocity);
+        float maxSpeed = _player.maxSpeed * _player.maxSpeed;
+        
+        if (currSpeed > maxSpeed)
+        {
+            float brakeSpeed = currSpeed - maxSpeed;  // calculate the speed decrease
+
+            Vector2 normalisedVelocity = _player.RB.velocity.normalized;
+            Vector2 brakeVelocity = normalisedVelocity * brakeSpeed;  // make the brake Vector2 value
+
+            _player.RB.AddForce(-brakeVelocity, ForceMode2D.Force);  // apply opposing brake force
+        }
     }
 }
