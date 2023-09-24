d775ce66e1353359688f8a55ccc9f02736a00c57
flowerflora <preciousmonkey2004@gmail.com>
2023-09-23 22:59:53 -0400

    Added basic animations + triggers for collectible

--- Assets/Scripts/Interaction/collectible/collectible.cs
+++ Assets/Scripts/Interaction/collectible/collectible.cs
@@ -8,6 +8,7 @@ public class collectible : AInteractable
 
     public override void Interact(PlayerInstance player)
     {
+        gameObject.GetComponent<Animator>().SetBool("IsCollected",true);
         Destroy(gameObject);
     }
 }
