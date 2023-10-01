501a95e9d1d2ef3fcd0f9aada66491fc5d7bd662
ShepherdGH <yuxinmiao222@gmail.com>
2023-10-01 10:32:04 -0400

    Finalized Scene Transition

--- Assets/Scripts/Interaction/SceneTransition/SceneTransition.cs
+++ Assets/Scripts/Interaction/SceneTransition/SceneTransition.cs
@@ -9,16 +9,21 @@ public class SceneTransition : AInteractable
 {
     public static List<SceneTransition> all = new List<SceneTransition>();
     public static List<int> sceneHistory = new List<int>();
-
     public enum SpawnLocation
     {
         Left,
         Right
     };    
-    
+    [Tooltip("Scene build index of the target scene")]
     [SerializeField] private int toScene;
+    
+    [Tooltip("Scene build index of the current scene")]
     [SerializeField] private int myScene;
+    
+    [Tooltip("Scene build index of the previous scene")]
     [SerializeField] public int fromScene;
+    
+    [Tooltip("Which side the player lands")]
     [SerializeField] public SpawnLocation spawnLocation = new SpawnLocation();
     
     
--- Assets/Scripts/Interaction/SceneTransition/TransitionManager.cs
+++ Assets/Scripts/Interaction/SceneTransition/TransitionManager.cs
@@ -8,27 +8,24 @@ public class TransitionManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public PlayerInstance player;
+    [Tooltip("The distance the player spawns from this entrence")]
+    [SerializeField] private float spawnDistance = 1.5f;
     
     void Start()
     {
-        Debug.Log("Manager Ran");
         if (SceneTransition.sceneHistory.Count > 1)
         {
-            Debug.Log("If Valid1");
-            Debug.Log(SceneTransition.all.Last().fromScene);
             foreach (SceneTransition st in SceneTransition.all)
             {
-                Debug.Log(st.fromScene);
                 if (st.fromScene == SceneTransition.sceneHistory.Last())
                 {
-                    Debug.Log("If Valid2");
                     if (st.spawnLocation == SceneTransition.SpawnLocation.Left)
                     {
-                        player.transform.position = st.transform.position + new Vector3(-3, 0, 0);
+                        player.transform.position = st.transform.position + new Vector3(-spawnDistance, 0, 0);
                     }
                     else
                     {
-                        player.transform.position = st.transform.position + new Vector3(3, 0, 0);
+                        player.transform.position = st.transform.position + new Vector3(spawnDistance, 0, 0);
                     }
                     
                 }
