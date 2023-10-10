c62d097af16463512d34045a1e6b2ef3ffbe9808
Anika <Slorchthefirst@gmail.com>
2023-10-10 18:36:37 -0400

    added fadeout effect

--- Assets/Scripts/UI/Dialogue/FadeEffect.cs
+++ Assets/Scripts/UI/Dialogue/FadeEffect.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using TMPro;
 public class FadeEffect : MonoBehaviour
 {
-    [SerializeField] private float fadetime = 1f;
+    [SerializeField] private float FadeInTime = 0.5f;
+
+    [SerializeField] private float FadeOutTime = 0.5f;
     public Coroutine Run(string textToType, TMP_Text textLabel)
     {
         textLabel.CrossFadeAlpha(0,0.0f,false);
@@ -19,13 +21,13 @@ public class FadeEffect : MonoBehaviour
 private IEnumerator fadein(string textToType, TMP_Text textLabel)
     {
         textLabel.text = textToType;
-        textLabel.CrossFadeAlpha(1,fadetime,false);
+        textLabel.CrossFadeAlpha(1,FadeInTime,false);
     yield return null;
     }
 
 private IEnumerator fadeout(string textToType, TMP_Text textLabel)
     {
-    textLabel.CrossFadeAlpha(0,fadetime,false);
+    textLabel.CrossFadeAlpha(0,FadeOutTime,false);
     yield return null;
     }
 }
