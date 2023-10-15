da01d5b9a302019326ac8510ebd4672c66471f3e
C <ray20000309@icloud.com>
2023-10-15 17:26:27 -0400

    Pause Menu controller

--- Assets/Scripts/UI/Menu/PauseMenuController.cs
+++ Assets/Scripts/UI/Menu/PauseMenuController.cs
@@ -2,30 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class PauseMenuController : MonoBehaviour
 {
-    public GameObject _pauseMenuCanvasGO;
-    public GameObject _normalCanvasGO;
+    private GameObject _pauseMenuCanvasGO;
+    private GameObject _normalCanvasGO;
+
+    private GameObject _selectedFirst;
+
+    private GameObject _resumeButton;
+    private GameObject _backToMainButton;
 
     private bool isPaused;
 
     private void Start()
     {
-        _pauseMenuCanvasGO.SetActive(false);
+        _pauseMenuCanvasGO = GameObject.Find("pauseMenuCanvas") ;
+        _normalCanvasGO = GameObject.Find("normalCanvas");
 
+        _resumeButton = GameObject.Find("ResumeButton");
+        _selectedFirst = _resumeButton;
+        _backToMainButton = GameObject.Find("BackToMainButton");
+
+
+
+        _pauseMenuCanvasGO.SetActive(false);
+        _normalCanvasGO.SetActive(true);
     }
 
-    private void Updated()
+    public void OpenCloseMenu(InputAction.CallbackContext context)
     {
-        // if(PlayerInput.MenuOpenCloseInput)
-        // {
-        //     if(!isPaused){
-        //         Pause();
-        //     }else{
-        //         Unpause();
-        //     }
-        // }
+        if (context.performed)
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Unpause();
+            }
+        }
     }
 
     public void Pause(){
@@ -38,15 +57,19 @@ public class PauseMenuController : MonoBehaviour
     public void Unpause(){
         isPaused=false;
         Time.timeScale=1f;
-        CloseAllMenu();
+        ClosePauseMenu();
     }
     public void OpenPauseMenu(){
         _pauseMenuCanvasGO.SetActive(true);
         _normalCanvasGO.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(_selectedFirst);
+
     }
-    public void CloseAllMenu(){
+    public void ClosePauseMenu(){
         _pauseMenuCanvasGO.SetActive(false);
         _normalCanvasGO.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
     }
 
     public void OnResumePress(){
@@ -55,5 +78,17 @@ public class PauseMenuController : MonoBehaviour
     public void OnEscPress(){
         Pause();
     }
+    public void OnBackToMainPress() {
+        SceneManager.LoadScene(sceneName: "mainMenu");
+    }
+    public void onResumeOver()
+    {
+        EventSystem.current.SetSelectedGameObject(_resumeButton);
+    }
+    public void onBackToMainOver()
+    {
+        EventSystem.current.SetSelectedGameObject(_backToMainButton);
+    }
+    
 
 }
