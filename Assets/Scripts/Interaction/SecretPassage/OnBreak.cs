d5baf5f05c7e1646f6d1e26753f8f87e82d6f6e5
ShepherdGH <yuxinmiao222@gmail.com>
2023-11-22 00:58:51 -0500

    Added fading animation to the two scripts

--- Assets/Scripts/Interaction/SecretPassage/OnBreak.cs
+++ Assets/Scripts/Interaction/SecretPassage/OnBreak.cs
@@ -7,12 +7,26 @@ using UnityEngine;
 public class OnBreak : MonoBehaviour
 {
     [SerializeField] private GameObject breakObject;
-
+    public float fadeSpeed = 3;
     private void Update()
     {
+
+        
         if (!breakObject)
         {
-            Destroy(gameObject);
+            Color _objectColor = gameObject.GetComponent<Renderer>().material.color;
+            float _fadeAmount = _objectColor.a - (fadeSpeed * Time.deltaTime);
+
+            _objectColor = new Color(_objectColor.r, _objectColor.g, _objectColor.b, _fadeAmount);
+            gameObject.GetComponent<Renderer>().material.color = _objectColor;
+
+            if (_objectColor.a <= 0)
+            {
+                Destroy(gameObject);
+            }
+            
         }
-    }   
+    }
+
+
 }
