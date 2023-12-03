daba66baf46b9000bdb63ae6884c42ef8300578f
Shangyi Zhu <62895460+pufferFish-sz@users.noreply.github.com>
2023-12-03 16:54:18 -0500

    UI and Animation Sounds
    Added a singleton script to the audio manager for it to have instance

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : Singleton<AudioManager>
 {
     [Header("Character Animation Sounds")]
     [SerializeField] public AK.Wwise.Event singleJump;
     [SerializeField] public AK.Wwise.Event doubleJump;
     [SerializeField] public AK.Wwise.Event grassFootStep;
     [SerializeField] public AK.Wwise.Event attack;
+    [SerializeField] public AK.Wwise.Event playerDeath;
+
+    [Header("UI Sounds")]
+    [SerializeField] public AK.Wwise.Event click;
+    [SerializeField] public AK.Wwise.Event buttonPress;
 
 
     void playSingleJump()
@@ -31,6 +36,20 @@ public class AudioManager : MonoBehaviour
         attack.Post(gameObject);
     }
 
+    void playPlayerDeath()
+    {
+        playerDeath.Post(gameObject);
+    }
+
+    public void playClick()
+    {
+        click.Post(gameObject);
+    }
+
+    public void playButtonPress()
+    {
+        buttonPress.Post(gameObject);
+    }
 
 
 
