62b60a6bfb2925e9378180f22cb969cb45230836
superpi2 <superpi2.Aa.Xx@gmail.com>
2023-12-03 14:39:50 -0500

    Walls shake when hit, starting dialogue box

--- Assets/Scripts/UI/Dialogue/PromptTrigger.cs
+++ Assets/Scripts/UI/Dialogue/PromptTrigger.cs
@@ -7,12 +7,18 @@ public class PromptTrigger : AInteractable
     [SerializeField] DialogueObject _dialogueObject;
     [SerializeField] DialogueUI _dialogueUI;
     [SerializeField] private Vector2 offset;
-    [SerializeField] bool activeImmediately;
+    [SerializeField] bool startupDialogue;
 
     public void Start()
     {
-        if (activeImmediately)
-            Interact(FindObjectOfType<PlayerInstance>());
+        if (startupDialogue || InitialiseData.initialised)
+            StartCoroutine(Activate());
+    }
+
+    IEnumerator Activate()
+    {
+        yield return new WaitForEndOfFrame();
+        Interact(FindObjectOfType<PlayerInstance>());
     }
 
     public override void OnExit(PlayerInstance player)
@@ -25,5 +31,9 @@ public class PromptTrigger : AInteractable
     public override void Interact(PlayerInstance player)
     {
         _dialogueUI.ShowDialogue(_dialogueObject, transform.position, (Vector3)offset);
+        if (_dialogueObject._StopPlayer)
+        {
+            player.controller.ToggleActionMap(player.controller.inputActions.Dialogue);
+        }
     }
 }
