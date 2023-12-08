1d88e09843f701994c0d81e47c7832f803375a30
Eiekama <42797046+Eiekama@users.noreply.github.com>
2023-12-08 15:44:05 -0500

    added enemy animations

--- Assets/Scripts/Enemy/Slime/SlimeManager.cs
+++ Assets/Scripts/Enemy/Slime/SlimeManager.cs
@@ -15,10 +15,13 @@ public class SlimeManager : AStateManager
     public Path path;
     public float dist = 5f;
 
+    public Animator anim { get; private set; }
+
     private void Awake()
     {
         seeker = GetComponent<Seeker>();
         RB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         dashState.manager = this;
         idleState.manager = this;
 
